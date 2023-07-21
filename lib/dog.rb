ba652dca1be94ee1e7b895bf0baf105e8f73225d
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-07-21 04:17:20 +0300

    Done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,28 @@
-class Dog
+class Dog < ActiveRecord::Base 
+  
+  def self.create(attributes)
+    dog = self.new(attributes)
+    dog.save
+    dog
+  end
+  def save
+    super
+    self.id = self.class.last.id if self.id.nil?
+    self
+  end
+
+  def update(attributes)
+    self.update(attributes)
+  end
+  
+  def self.find_or_create_by(attributes)
+    self.find_by(attributes) || self.create(attributes)
+  end
+  def self.find_by_name(name)
+    self.find_by(name: name)
+  end
+  def self.find_by_id(id)
+    self.find(id)
+  end
 end
+
